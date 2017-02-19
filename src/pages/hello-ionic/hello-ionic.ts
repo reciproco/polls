abd52aa7596b6f3116eadc80a91a52272a0b9e10
reciproco <reciproco@gmail.com>
2017-02-19 22:20:47 +0100

    Primera version funcionando completa

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { Content } from 'ionic-angular';
 
 
 declare var $:any;
@@ -9,56 +10,101 @@ declare var ReconnectingWebSocket:any;
   templateUrl: 'hello-ionic.html'
 })
 export class HelloIonicPage {
+  private socket;
+  @ViewChild(Content) content: Content;
+  p1: number;
+  p2: number;
+  p3: number;
+  selected: number;
+
   constructor() {
+      this.p1 = 0;
+      this.p2 = 0;
+      this.p3 = 0;
+      this.selected  = 0;
+
+  }
+  ionViewWillEnter() { 
     var ws_scheme = window.location.protocol == "https:" ? "wss" : "ws";
     //var ws_path = ws_scheme + '://' + window.location.host + '/';
     var ws_path = ws_scheme + '://192.168.1.38:8000/';
     console.log("Connecting to " + ws_path)
-    var socket = new ReconnectingWebSocket(ws_path);
-    socket.debug = true;
-    socket.reconnectInterval = 100;
-    socket.maxReconnectInterval = 3000;
+    this.socket = new ReconnectingWebSocket(ws_path);
+    this.socket.debug = true;
+    this.socket.reconnectInterval = 100;
+    this.socket.maxReconnectInterval = 3000;
 
-    socket.onmessage = function(message) {
+    this.socket.onmessage = (message) => {
         console.log("Got message: " + message.data);
         var data = JSON.parse(message.data);
         // if action is started, add new item to table
+        if (data.action == "estadisticas") {
+            if(data.pregunta == 1) {
+                this.p1 = data.votos;
+            }
+            if(data.pregunta == 2) {
+                this.p2 = data.votos;
+            }
+            if(data.pregunta == 3) {
+                this.p3 = data.votos;
+            }
+        }
+
         if (data.action == "initial") {
-            var task_status = $("#task_status");
-            $('#pregunta1')
-            .css('background-color', '');
-            $('#pregunta2')
-            .css('background-color', '');
-            $('#pregunta3')
-            .css('background-color', '');
-            $('#pregunta' + data.pregunta)
+            this.selected = data.pregunta;
+            $('#card1')
             .css('background-color', '')
-            .css('background-color', 'blue');
-            $("#pregunta").val(data.pregunta);
+            $('#card2')
+            .css('background-color', '')
+            $('#card3')
+            .css('background-color', '')
+            $('#card' + data.pregunta)
+            .css('background-color', 'LightBlue')
+            if(data.pregunta==3){
+                this.scrollTo(650);
+            } else if(data.pregunta==2) {
+                this.scrollTo(390);
+            } else {
+                this.scrollTo(0);
+            }
         }
         // if action is completed, just update the status
         else if (data.action == "completed"){
-            $('#pregunta1')
-            .css('background-color', '');
-            $('#pregunta2')
-            .css('background-color', '');
-            $('#pregunta3')
-            .css('background-color', '');
-            $('#pregunta' + data.pregunta)
+            this.selected = data.pregunta;
+            $('#card1')
             .css('background-color', '')
-            .css('background-color', 'blue');
-            $("#pregunta").val(data.pregunta);
+            $('#card2')
+            .css('background-color', '')
+            $('#card3')
+            .css('background-color', '')
+            $('#card' + data.pregunta)
+            .css('background-color', 'LightBlue')
+            if(data.pregunta==3){
+                this.scrollTo(650);
+            } else if(data.pregunta==2) {
+                this.scrollTo(390);
+            } else {
+                this.scrollTo(0);
+            }
         }
     };
-    $("#taskform").on("submit", function(event) {
-        var message = {
-            pregunta: $('#pregunta').val() ,
-            votos: $('#task_name').val()
-        };
-        socket.send(JSON.stringify(message));
-        return false;
-    });
+  }
 
+  vote(arg) {
+        if(arg == this.selected)
+        {
+            console.log("votando")
+            var message = {
+                pregunta: arg ,
+                votos: 1
+            };
+            this.socket.send(JSON.stringify(message));
+            return false;
+        }
+  }
 
+  scrollTo(y) {
+    console.log('scroll ' + y);
+    this.content.scrollTo(0, y, 200);
   }
 }
